8e9d3bd334782dde26783e10493964b591f65933
viethan <vietaing@gmail.com>
2021-01-03 00:23:13 +0200

    added

---
+++ 1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        dic[nums[0]] = 0 
+        
+        for i in range(1, len(nums)):
+            if target - nums[i] in dic:
+                return [dic[target - nums[i]], i]
+            else:
+                dic[nums[i]] = i
