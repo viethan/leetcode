f022ed7cda2009c25700ea01b045721341f8dd50
viethan <vietaing@gmail.com>
2021-02-02 09:17:37 +0200

    added 191.py

---
+++ 191.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        weight = 0
+        while n:
+            weight += n & 1
+            n = n >> 1
+            
+        return weight
